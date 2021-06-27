66af27f1c06ac3ccd02f9e30b7cb65f46b7f8e1f
pinomartin <martin91pino@gmail.com>
2021-06-26 21:38:30 -0300

    jumbotron to Home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,19 +1,21 @@
 import MainLogo from "../assets/mainLogoTransparent.svg";
 import Footer from "./Footer";
 
-
-
 const Home = () => {
   return (
     <>
-    <div className="mt-5 text-center">
-      <img src={MainLogo} alt="" className="img-fluid"/>
-      <hr />
-      <h3 className="text-white">
-        {"Bienvenido a una nueva forma de trabajo Freelance"}
-      </h3>      
-    </div>
-    <Footer />
+      <div className="jumbotron jumbotron-fluid">
+        <div className="container">
+          <div className="mt-5 text-center">
+            <img src={MainLogo} alt="" className="img-fluid" />
+            <hr />
+            <h3 className="text-white">
+              {"Bienvenido a una nueva forma de trabajo Freelance"}
+            </h3>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </>
   );
 };
